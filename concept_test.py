6f4a6f187379e1a13e7c6d6deb8adfa34a090d60
odoluca <osman.doluca@iue.edu.tr>
2017-12-29 14:47:45 +0300

    Working PBM and PB patterns

--- concept_test.py
+++ concept_test.py
@@ -1,26 +1,36 @@
 #inspired by ImGQfinder algorithm at http://imgqfinder.niifhm.ru/algorithm/
 import re
 import regex
+import string
 sequence="""
-GGGTTTGGGTTTGGGTTTGGGTTTTTTTTTTTGGTGTTTGGGTTTGGGTTTGGGTTTTTTTTTTTTGGGTTTGGGTTTGGGTTTGGGTTTTTTTTTTGGTGTTTGGGTTTGGGTTTGGGTTTGGTG
+GGGTTTGGGTTTGGGTT TGGGT
+TTTTTTTTTTGGTGTTTGGGTTTGGGTTTGGTGTTTTTTTTTTTTGTGTTTGGGTTTGGGTTTGGGTTTTTTTTTTGGTGTTTGGGTTTGGGTTTGGGTTTGGTG
 """
 
 
-G3_PMB_greedy_pattern=r"""  ( [G]{3,} | (?P<mis>[G]{1,}[ATC][G]{1,})) (\w{1,7}  (?(mis)[G]{3,}| ([G]{3,}|(?P<mis>[G]{1,}[ATC][G]{1,})) )){3,} """
-#finds all perfect, mismatched or buldged imperfect pG4s
+G3_L17_PMB_greedy_pattern=r"""  ( [G]{3,} | (?P<mis>[G]{1,}[ATC][G]{1,})) (\w{1,7}  (?(mis)[G]{3,}| ([G]{3,}|(?P<mis>[G]{1,}[ATC][G]{1,})) )){3,} """
+C3_L17_PMB_greedy_pattern=r"""  ( [C]{3,} | (?P<mis>[C]{1,}[ATG][C]{1,})) (\w{1,7}  (?(mis)[C]{3,}| ([C]{3,}|(?P<mis>[C]{1,}[ATG][C]{1,})) )){3,} """
+#finds al perfect, mismatched or buldged imperfect pG4s
 
-G3_PB_greedy_pattern=r"""  ( [G]{3,} | (?P<mis>[G]{2,}[ATC][G]{1,}|[G]{1,}[ATC][G]{2,})) (\w{1,7}  (?(mis)[G]{3,}| ([G]{3,}|(?P<mis>[G]{2,}[ATC][G]{1,}|[G]{1,}[ATC][G]{2,})) )){3,} """
+
+G3_L17_PB_greedy_pattern=r"""  ( [G]{3,} | (?P<mis>[G]{2,}[ATC][G]{1,}|[G]{1,}[ATC][G]{2,})) (\w{1,7}  (?(mis)[G]{3,}| ([G]{3,}|(?P<mis>[G]{2,}[ATC][G]{1,}|[G]{1,}[ATC][G]{2,})) )){3,} """
+C3_L17_PB_greedy_pattern=r"""  ( [C]{3,} | (?P<mis>[C]{2,}[ATG][C]{1,}|[C]{1,}[ATG][C]{2,})) (\w{1,7}  (?(mis)[C]{3,}| ([C]{3,}|(?P<mis>[C]{2,}[ATG][C]{1,}|[C]{1,}[ATG][C]{2,})) )){3,} """
 #finds both perfect or bulged imperfect pG4s.
 
+#G3_L17_PMB_greedy_pattern=r"""  ( [G]{3,} | (?P<mis>[G]{1,}[\w][G]{1,})) (\w{1,7}  (?(mis)[G]{3,}| ([G]{3,}|(?P<mis>[G]{1,}[\w][G]{1,})) )){3,} """
 
-prog=regex.Regex(G3_PB_greedy_pattern,regex.VERBOSE|regex.MULTILINE)
+prog=regex.Regex(G3_L17_PB_greedy_pattern,regex.VERBOSE|regex.MULTILINE)
 
 
-def func():
+def func(sequence):
     result = prog.finditer(sequence)
     return result
 
-result=func()
+sequence=sequence.translate(string.maketrans("","")," \n")
+#sequence=sequence.replace("\n","")
+
+result=func(sequence)
+
 print sequence
 for m in result:
     print " "*(m.start()-1)+sequence[m.start():m.end()]
