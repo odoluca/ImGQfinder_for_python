e8de67d0e5dac5e29bb5413d89e361ee968e3135
odoluca <osman.doluca@iue.edu.tr>
2017-12-29 13:46:20 +0300

    Working PBM and PB patterns

--- concept_test.py
+++ concept_test.py
@@ -2,23 +2,18 @@
 import re
 import regex
 sequence="""
-GGGTTTGGGTTTGGGTTTGGGTTTTTTTTTTTTGGTGTTTGGGTTTGGGTTTGGGTTTTTTTTTTTTGGGTTTGGGTTTGGGTTTGTGTTTTTTTTTTGGTGTTTGGGTTTGGGTTTGGGTTTGGTG
+GGGTTTGGGTTTGGGTTTGGGTTTTTTTTTTTGGTGTTTGGGTTTGGGTTTGGGTTTTTTTTTTTTGGGTTTGGGTTTGGGTTTGGGTTTTTTTTTTGGTGTTTGGGTTTGGGTTTGGGTTTGGTG
 """
 
 
 G3_PMB_greedy_pattern=r"""  ( [G]{3,} | (?P<mis>[G]{1,}[ATC][G]{1,})) (\w{1,7}  (?(mis)[G]{3,}| ([G]{3,}|(?P<mis>[G]{1,}[ATC][G]{1,})) )){3,} """
-#G3_PB_greedy_pattern=r"""( [G]{3,} | (?P<mis>([G]{2,}[ATC][G]{1,}|[G]{1,}[ATC][G]{2,}))) ( \w{1,7} (?(mis)[G]{3,} | (?P<mis>([G]{2,}[ATC][G]{1,}|[G]{1,}[ATC][G]{2,})) )){2,} ( \w{1,7} (?(mis)[G]{3,} | ([G]{2,}[ATC][G]{1,}|[G]{1,}[ATC][G]{2,}|[G]{3,}) ))"""
-
-#G3_PMB_nongreedy_pattern=r"""( [G]{3,} | (?P<mis>[G]{1,}[ATC][G]{1,})) ( \w{1,7} (?(mis)[G]{3,} | (?P<mis>[G]{1,}[ATC][G]{1,}) )){3} """
-
-
-
-prog=regex.Regex(G3_PMB_greedy_pattern,regex.VERBOSE|regex.MULTILINE)
 #finds all perfect, mismatched or buldged imperfect pG4s
 
-
-#prog=regex.Regex(G3_PB_greedy_pattern,regex.VERBOSE|regex.MULTILINE)
+G3_PB_greedy_pattern=r"""  ( [G]{3,} | (?P<mis>[G]{2,}[ATC][G]{1,}|[G]{1,}[ATC][G]{2,})) (\w{1,7}  (?(mis)[G]{3,}| ([G]{3,}|(?P<mis>[G]{2,}[ATC][G]{1,}|[G]{1,}[ATC][G]{2,})) )){3,} """
 #finds both perfect or bulged imperfect pG4s.
+
+
+prog=regex.Regex(G3_PB_greedy_pattern,regex.VERBOSE|regex.MULTILINE)
 
 
 def func():
@@ -28,5 +23,5 @@ def func():
 result=func()
 print sequence
 for m in result:
-    print " "*(m.start()-1),sequence[m.start():m.end()]
+    print " "*(m.start()-1)+sequence[m.start():m.end()]
 
