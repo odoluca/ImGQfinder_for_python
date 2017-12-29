3a817683e332e9ccc8a3b717d996a94118f3ecff
odoluca <osman.doluca@iue.edu.tr>
2017-12-29 12:02:41 +0300

    ignore file

--- concept_test.py
+++ concept_test.py
@@ -1,13 +1,12 @@
 #inspired by ImGQfinder algorithm at http://imgqfinder.niifhm.ru/algorithm/
 import re
 import regex
-sequence="TTTGGGTTTTTGTGGTTTGGGTTTTTGGGTTTGGTGTTTTTGTGG"
+sequence="TTTGGGTTTTTGGGTTTGGGTTTTTGGGTTTGGGTTTTTGGG"
 
 G3_PMB_greedy_pattern=r"""( [G]{3,} | (?P<mis>[G]{1,}[ATC][G]{1,})) ( \w{1,7} (?(mis)[G]{3,} | (?P<mis>[G]{1,}[ATC][G]{1,}) )){3,} """
-G3_PB_greedy_pattern=r"""( [G]{3,} | (?P<mis>([G]{2,}[ATC][G]{1,}|[G]{1,}[ATC][G]{2,}))) ( \w{1,7} (?(mis)[G]{3,} | (?P<mis>([G]{2,}[ATC][G]{1,}|[G]{1,}[ATC][G]{2,})) )){3,} """
+G3_PB_greedy_pattern=r"""( [G]{3,} | (?P<mis>([G]{2,}[ATC][G]{1,}|[G]{1,}[ATC][G]{2,}))) ( \w{1,7} (?(mis)[G]{3,} | (?P<mis>([G]{2,}[ATC][G]{1,}|[G]{1,}[ATC][G]{2,})) )){2,} ( \w{1,7} (?(mis)[G]{3,} | ([G]{2,}[ATC][G]{1,}|[G]{1,}[ATC][G]{2,}|[G]{3,}) ))"""
 
-G3_PMB_nongreedy_pattern=r"""( [G]{3,} | (?P<mis>[G]{1,}[ATC][G]{1,})) ( \w{1,7} (?(mis)[G]{3,} | (?P<mis>[G]{1,}[ATC][G]{1,}) )){3} """
-G3_PB_nongreedy_pattern=r"""( [G]{3,} | (?P<mis>([G]{2,}[ATC][G]{1,}|[G]{1,}[ATC][G]{2,}))) ( \w{1,7} (?(mis)[G]{3,} | (?P<mis>([G]{2,}[ATC][G]{1,}|[G]{1,}[ATC][G]{2,})) )){3} """
+#G3_PMB_nongreedy_pattern=r"""( [G]{3,} | (?P<mis>[G]{1,}[ATC][G]{1,})) ( \w{1,7} (?(mis)[G]{3,} | (?P<mis>[G]{1,}[ATC][G]{1,}) )){3} """
 
 
 
